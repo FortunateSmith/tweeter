628fa6af665bc782be2060155fd83108ca5f0310
Liam Smith <liam.s.smith@gmail.com>
2022-03-29 01:47:17 +0000

    add some descriptive comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,45 +3,13 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const tweetData = {
-//   user: {
-//     name: "Descartes",
-//     avatars: "https://i.imgur.com/nlhLi3I.png",
-//     handle: "@rd",
-//   },
-//   content: {
-//     text: "Je pense , donc je suis",
-//   },
-//   created_at: 1648310316916,
-// };
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
+
+
+
 
 // creates html element to be rendered from json object
-const createTweetElement = function (tweetData) {
+const createTweetElement = function(tweetData) {
   const $tweet = $(
     `<article class="tweet">
     <header>
@@ -66,21 +34,26 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 };
 
+
+
+
+
 // renders html created with createTweetElement function
-const renderTweet = function (data) {
+const renderTweet = function(data) {
+  //loop through json data
   data.forEach((tweetData) => {
-    // console.log(tweetData.content);
+    // store data of current index in loop
     const tweetEl = createTweetElement(tweetData);
+    // add to top of tweet-container
     $("section.tweet-container").prepend(tweetEl);
   });
 };
 
-// const form = document.getElementById("submit-form");
-// form.addEventListener('submit', () => {
-
-// });
 
 
+
+
+// async loading of new tweets (no page refresh)
 const loadTweets = () => {
   $.ajax({
     method: "GET",
@@ -92,30 +65,30 @@ const loadTweets = () => {
   });
 };
 
-$(document).ready(function () {
+
+
+// 
+$(document).ready(function() {
   console.log("ready!");
 
   loadTweets();
 
-  $("form").submit(function (event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-    console.log("Did this work?");
-
+    
+    // async request to post new tweets
     $.ajax({
       url: "/tweets",
       method: "POST",
       type: "application/json",
       data: $(this).serialize(),
-      success: function(serverRes) {
+      success: function() {
         $("textarea").val("");
         $.get("/tweets", data => {
           const newTweet = [data.slice(-1).pop()];
           renderTweet(newTweet);
-
-          console.log("Ajax successful!")
         });
       }
-    })
+    });
   });
-  
 });
