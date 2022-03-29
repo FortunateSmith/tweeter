580f7fda4cfc589d0de72040d49ad1db9e9fd803
Liam Smith <liam.s.smith@gmail.com>
2022-03-29 15:14:56 +0000

    fix bug in if statement so counter turns red when less than 0 chars, add comments for clarification

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,24 @@
-//let prevLengthOfTextarea = 0;
+// let prevLengthOfTextarea = 0;
 let defaultCharsLeft = 140;
+
 
 
 $(document).ready(function() {
   $("form").hide().show(1000)
     .find("textarea")
+    //count happens when key is released
     .keyup(function() {
       const charInTextarea = $(this).val().length;
       const charsLeft = defaultCharsLeft - charInTextarea;
-      if (charsLeft > defaultCharsLeft) {
+      if (charsLeft < 0) {
         ["-", charsLeft].join("");
+        // this refers to <textarea>
         $(this)
           .next()
           .find("output")
           .text(charsLeft)
           .css("color", "red");
+          console.log(this)
       }
       $(this)
         .next()
